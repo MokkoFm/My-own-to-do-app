5be13a81f6c54064a04a3f5282139c4415bac3b0
MokkoFm <mokkofmpoetry@gmail.com>
2020-10-19 00:48:16 +0300

    add update, delete pages

---
+++ tasks/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Task
+from .forms import TaskForm
+
+# Create your views here.
+def index(request):
+    tasks = Task.objects.all()
+    form = TaskForm()
+
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect("/")
+
+    context = {
+        'tasks': tasks,
+        'form': form,
+    }
+    return render(request, 'index.html', context=context)
+
+
+def update_task(request, pk):
+    task = Task.objects.get(id=pk)
+    form = TaskForm(instance=task)
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+        return redirect("/")
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'update_task.html', context=context)
+
+
+def delete_task(request, pk):
+    task = Task.objects.get(id=pk)
+    context = {
+        'task': task,
+    }
+    return render(request, 'delete.html', context=context)
